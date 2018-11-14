7d86403890c23c6c935e1e8580b1c0dfef1631d1
heatlee <oldmanfak@gmail.com>
2018-11-14 11:02:20 +0300

    Calendar data class

--- src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task01/model/logic/Counter.java
+++ src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task01/model/logic/Counter.java
@@ -2,33 +2,35 @@ package by.bntu.fitr.povt.justcompileit.javalabs.lab07.task01.model.logic;
 
 public class Counter {
 
-    public static final int DEFAULT_NUMBER_OF_HEADS = 3;
-    public static final int FIRST_PERIOD = 200;
-    public static final int SECOND_PERIOD = 300;
-    public static final int INCRIMENT_ON_FIRST_PERIOD = 3;
-    public static final int INCRIMENT_ON_SECOND_PERIOD = 2;
-    public static final int INCRIMENT_THIRD_PERIOD = 1;
+    private static final int DEFAULT_NUMBER_OF_HEADS = 3;
+    private static final int FIRST_PERIOD = 200;
+    private static final int SECOND_PERIOD = 300;
+    private static final int INCRIMENT_ON_FIRST_PERIOD = 3;
+    private static final int INCRIMENT_ON_SECOND_PERIOD = 2;
+    private static final int INCRIMENT_THIRD_PERIOD = 1;
 
     public static int countHeads(int amountOfYears) {
 
         int headsCount = 0;
 
-        if (amountOfYears <= 0) {
-            return headsCount;
-        }
-        if (amountOfYears < FIRST_PERIOD) {
+        if (amountOfYears > 0) {
 
-            headsCount = DEFAULT_NUMBER_OF_HEADS + amountOfYears * INCRIMENT_ON_FIRST_PERIOD;
+            if (amountOfYears < FIRST_PERIOD) {
 
-        } else if (amountOfYears < SECOND_PERIOD) {
-            headsCount = DEFAULT_NUMBER_OF_HEADS + FIRST_PERIOD * INCRIMENT_ON_FIRST_PERIOD -
-                    FIRST_PERIOD * INCRIMENT_ON_SECOND_PERIOD + INCRIMENT_ON_SECOND_PERIOD * amountOfYears;
-        } else {
+                headsCount = DEFAULT_NUMBER_OF_HEADS + amountOfYears * INCRIMENT_ON_FIRST_PERIOD;
 
-            headsCount = DEFAULT_NUMBER_OF_HEADS + FIRST_PERIOD * INCRIMENT_ON_FIRST_PERIOD +
-                    SECOND_PERIOD * INCRIMENT_ON_SECOND_PERIOD - SECOND_PERIOD * INCRIMENT_THIRD_PERIOD +
-                    amountOfYears * INCRIMENT_THIRD_PERIOD;
+            } else if (amountOfYears < SECOND_PERIOD) {
+                headsCount = DEFAULT_NUMBER_OF_HEADS + FIRST_PERIOD + amountOfYears * 2;
+                //headsCount = DEFAULT_NUMBER_OF_HEADS + FIRST_PERIOD * INCRIMENT_ON_FIRST_PERIOD -
+                        //FIRST_PERIOD * INCRIMENT_ON_SECOND_PERIOD + INCRIMENT_ON_SECOND_PERIOD * amountOfYears;
+            } else {
+                headsCount = DEFAULT_NUMBER_OF_HEADS + FIRST_PERIOD + SECOND_PERIOD +
+                        amountOfYears * INCRIMENT_THIRD_PERIOD;
+//                headsCount = DEFAULT_NUMBER_OF_HEADS + FIRST_PERIOD * INCRIMENT_ON_FIRST_PERIOD +
+//                        SECOND_PERIOD * INCRIMENT_ON_SECOND_PERIOD - SECOND_PERIOD * INCRIMENT_THIRD_PERIOD +
+//                        amountOfYears * INCRIMENT_THIRD_PERIOD;
 
+            }
         }
 
         return headsCount;
