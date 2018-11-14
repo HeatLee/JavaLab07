47042fb7f7f8555a27fa136e96ae0164d1e8767a
heatlee <oldmanfak@gmail.com>
2018-11-14 11:37:37 +0300

    Mood data class

--- src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task03/controller/MoodSensor.java
+++ src/by/bntu/fitr/povt/justcompileit/javalabs/lab07/task03/controller/MoodSensor.java
@@ -2,12 +2,15 @@ package by.bntu.fitr.povt.justcompileit.javalabs.lab07.task03.controller;
 
 import by.bntu.fitr.povt.justcompileit.javalabs.lab07.task03.view.Printer;
 import by.bntu.fitr.povt.justcompileit.javalabs.lab07.task03.model.logic.MoodDetecter;
+import by.bntu.fitr.povt.justcompileit.javalabs.lab07.task03.model.data.DataStore;
 
 public class MoodSensor {
 
+    private static final DataStore dataStore = new DataStore();
+
     public static void main(String[] args) {
 
-        String face = MoodDetecter.detectMood();
+        String face = MoodDetecter.detectMood(dataStore);
 
         Printer.println("Your mood: ");
         Printer.println(face);
